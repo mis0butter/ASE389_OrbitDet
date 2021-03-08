2024ef0505f49ac8d7fdb9eefa35494b8cfadd7b
Hsin <Junette.Hsin@sslmda.com>
2021-03-08 00:21:47 -0800

    moved subfns into subspace +fn folder

---
+++ HW3/HW3.m
@@ -0,0 +1,33 @@
+% ASE 389 Orbit Determination
+% HW 2
+% Junette Hsin 
+
+clear; 
+
+addpath(genpath('fns')) 
+
+% positionA = [100 100 600 600]; 
+% positionB = [100 100 700 900]; 
+
+%% Problem 1 
+
+% Assume an orbit plane coordinate system with a gravitational parameter of 1, i.e., µ = 1. The
+% equations of motion are:
+% x¨ = −
+% x
+% r
+% 3
+% y¨ = −
+% y
+% r
+% 3
+% r
+% 2 = x
+% 2 + y
+% 2
+
+
+%%     
+%% subfunctions 
+
+
